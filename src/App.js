ad01f238a04f4701889b8d114bb4a7f1d8050e9e
Hunter K <huntertkoehn@gmail.com>
2022-11-28 17:19:48 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='login-screen'>
+      <div className='sign-in'>
+        <label>Username:</label>
+        <input type='text'></input>
+        <label className='label-two'>Password:</label>
+        <input className='input-two' type='text'></input>
+        <button>Login</button>
+      </div>
     </div>
   );
 }
